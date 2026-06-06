58240f8af99296a4b61c84e4fd7932b778ea9feb
rawrrudy <rudrasundgikar@gmail.com>
2026-06-06 13:50:48 +0530

    Update tds_test.ino

--- hardware/firmware/tests/tds_test.ino
+++ hardware/firmware/tests/tds_test.ino
@@ -1,1 +1,25 @@
+#define TDS_PIN 35
 
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  int adcValue = analogRead(TDS_PIN);
+
+  float voltage = adcValue * (3.3 / 4095.0);
+
+  float tdsValue = 
+      (133.42 * voltage * voltage * voltage
+      -255.86 * voltage * voltage
+      +857.39 * voltage) * 0.5;
+
+  Serial.print("Voltage: ");
+  Serial.print(voltage, 2);
+
+  Serial.print(" V  TDS: ");
+  Serial.print(tdsValue);
+  Serial.println(" ppm");
+
+  delay(1000);
+}
