4e34d3c1624316c1a38f5732d34bb31c673d52d4
rawrrudy <rudrasundgikar@gmail.com>
2026-06-06 13:44:42 +0530

    Update ph_test.ino

--- hardware/firmware/tests/ph_test.ino
+++ hardware/firmware/tests/ph_test.ino
@@ -1,1 +1,20 @@
+#define PH_PIN 34
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  int adcValue = analogRead(PH_PIN);
+
+  float voltage = adcValue * (3.3 / 4095.0);
+
+  Serial.print("ADC: ");
+  Serial.print(adcValue);
+
+  Serial.print("  Voltage: ");
+  Serial.println(voltage, 3);
+
+  delay(1000);
+}
 
