6c8f28a199e0fe7fb715ea5fbe86cbc1b1d6841f
rawrrudy <rudrasundgikar@gmail.com>
2026-06-06 13:57:20 +0530

    Update turbidity_test.ino

--- hardware/firmware/tests/turbidity_test.ino
+++ hardware/firmware/tests/turbidity_test.ino
@@ -1,1 +1,20 @@
+#define TURBIDITY_PIN 32
 
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+
+  int adcValue = analogRead(TURBIDITY_PIN);
+
+  float voltage = adcValue * (3.3 / 4095.0);
+
+  Serial.print("ADC: ");
+  Serial.print(adcValue);
+
+  Serial.print(" Voltage: ");
+  Serial.println(voltage);
+
+  delay(1000);
+}
